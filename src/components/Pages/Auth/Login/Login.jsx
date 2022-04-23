b7945cbe47c41f3567dd0bffc2a87782ebd7dc26
iinaamasum <iinaamasum@gmail.com>
2022-04-23 13:40:29 +0600

    login register functionality partially done

--- src/components/Pages/Auth/Login/Login.jsx
+++ src/components/Pages/Auth/Login/Login.jsx
@@ -1,6 +1,11 @@
-import React, { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import {
+  useAuthState,
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from 'react-firebase-hooks/auth';
+import toast, { Toaster } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../../firebase.init';
 import Footer from '../../../Shared/Footer/Footer';
 import Nav from '../../../Shared/Nav/Nav';
@@ -11,6 +16,91 @@ const Login = () => {
   const location = useLocation();
   const [user] = useAuthState(auth);
   const from = location.state?.from?.pathname || '/';
+
+  const [signInWithEmailAndPassword, userField, loadingField, errorField] =
+    useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    emailError: '',
+    othersError: '',
+  });
+
+  const handleEmail = (e) => {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (regexEmail.test(e.target.value)) {
+      setUserInfo({ ...userInfo, email: e.target.value });
+      setErrors({ ...errors, emailError: '' });
+    } else {
+      setUserInfo({ ...userInfo, email: '' });
+      setErrors({ ...errors, emailError: 'Invalid Email' });
+    }
+  };
+
+  const handlePass = (e) => {
+    setUserInfo({ ...userInfo, password: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(userInfo.email, userInfo.password);
+  };
+
+  useEffect(() => {
+    const errors = errorField || error;
+    if (errors?.message.includes('auth/invalid-email')) {
+      toast.error('Invalid Email Entered!!!', {
+        icon: '❌',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+    } else if (errors?.message.includes('auth/wrong-password')) {
+      toast.error('Wrong Password!!!', {
+        icon: '❌',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+    } else if (errorField?.message.includes('auth/email-already-exists')) {
+      toast.error('User Exists!!!', {
+        icon: '❌',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+    } else if (errors?.message.includes('auth/user-not-found')) {
+      toast.error("You don't have an account!!! Please register first", {
+        icon: '❌',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+    } else if (errorField) {
+      toast.error(errors?.message, {
+        icon: '❌',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+    }
+  }, [errorField, error]);
 
   useEffect(() => {
     if (user) {
@@ -25,59 +115,77 @@ const Login = () => {
         <h2 className="text-gray-900 text-3xl text-center font-medium title-font mb-5">
           Log In
         </h2>
-
-        <div className="relative mb-4">
-          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            placeholder="Your Email"
-          />
-        </div>
-        <div className="relative mb-4">
-          <label htmlFor="password" className="leading-7 text-sm text-gray-600">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            placeholder="Your Password"
-          />
-        </div>
-        <div className="flex mb-5 text-sm justify-between">
-          <div className="flex justify-center items-center">
+        <Toaster />
+        <form onSubmit={handleSubmit}>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
             <input
-              className="mr-3 cursor-pointer"
-              type="checkbox"
-              name=""
-              id=""
+              onChange={handleEmail}
+              type="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="Your Email"
             />
-            <p>Remember Me</p>
-          </div>
-          <div className="">
-            <p>
-              Forgot Password?{' '}
-              <span className="underline text-blue-500 cursor-pointer font-bold">
-                Reset Now
-              </span>
+            <p className="text-red-700 text-xs pt-1 pl-1">
+              {errors?.emailError}
             </p>
           </div>
-        </div>
-        <div className="mb-1">
-          <p>
-            Don't have an account?{' '}
-            <span className="font-semibold text-blue-500 underline">
-              Create account
-            </span>
-          </p>
-        </div>
-        <button className="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
-          Log In
-        </button>
+          <div className="relative mb-4">
+            <label
+              htmlFor="password"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              onChange={handlePass}
+              type="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="Your Password"
+            />
+          </div>
+          <div className="flex mb-5 text-sm justify-between">
+            <div className="flex justify-center items-center">
+              <input
+                className="mr-3 cursor-pointer"
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>Remember Me</p>
+            </div>
+            <div className="">
+              <p>
+                Forgot Password?{' '}
+                <span
+                  onClick={() => sendPasswordResetEmail(userInfo.email)}
+                  className="underline text-blue-500 cursor-pointer font-bold"
+                >
+                  Reset Now
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="mb-1">
+            <p>
+              Don't have an account?{' '}
+              <Link
+                to="/register"
+                className="font-semibold text-blue-500 underline"
+              >
+                Create account
+              </Link>
+            </p>
+          </div>
+          <input
+            type="submit"
+            className="w-full cursor-pointer text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg"
+            value="Log In"
+          />
+        </form>
 
         <SocialAuth />
       </div>
--- src/components/Pages/Auth/Register/Register.jsx
+++ src/components/Pages/Auth/Register/Register.jsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  useAuthState,
+  useCreateUserWithEmailAndPassword,
+} from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../../firebase.init';
 import Footer from '../../../Shared/Footer/Footer';
 import Nav from '../../../Shared/Nav/Nav';
 import SocialAuth from '../SocialAuth/SocialAuth';
 
 const Register = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [user] = useAuthState(auth);
+  const from = location.state?.from?.pathname || '/';
+
+  const [createUserWithEmailAndPassword, userField, loadingField, errorField] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [errors, setErrors] = useState({
+    emailError: '',
+    passwordError: '',
+    confirmPasswordError: '',
+  });
+
+  const handleEmail = (e) => {
+    // console.log(e.target.value);
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (regexEmail.test(e.target.value)) {
+      setUserInfo({ ...userInfo, email: e.target.value });
+      setErrors({ ...errors, emailError: '' });
+    } else {
+      setUserInfo({ ...userInfo, email: '' });
+      setErrors({ ...errors, emailError: 'Invalid email' });
+    }
+  };
+  const handlePass = (e) => {
+    const regexPass = /(?=.*[!#$%&?^*@~() "])/;
+    if (regexPass.test(e.target.value)) {
+      setUserInfo({ ...userInfo, password: e.target.value });
+      setErrors({ ...errors, passwordError: '' });
+    } else {
+      setUserInfo({ ...userInfo, password: '' });
+      setErrors({
+        ...errors,
+        passwordError: 'At least 6 char including a special character.',
+      });
+    }
+  };
+
+  const handleConfirmPass = (e) => {
+    if (e.target.value === userInfo.password) {
+      setUserInfo({ ...userInfo, confirmPassword: e.target.value });
+      setErrors({ ...errors, confirmPasswordError: '' });
+    } else {
+      setUserInfo({ ...userInfo, confirmPassword: '' });
+      setErrors({ ...errors, confirmPasswordError: 'Password mismatched' });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userInfo.password === userInfo.confirmPassword) {
+      createUserWithEmailAndPassword(userInfo.email, userInfo.password);
+    } else {
+      setErrors({ ...errors, confirmPasswordError: 'Password mismatch' });
+      setUserInfo({ ...userInfo, confirmPassword: '' });
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate(from);
+    }
+  }, [user]);
   return (
     <>
       <Nav>col</Nav>
@@ -11,53 +87,75 @@ const Register = () => {
         <h2 className="text-gray-900 text-3xl text-center font-medium title-font mb-5">
           Register
         </h2>
-        <div className="mb-4">
-          <label
-            htmlFor="full-name"
-            className="leading-7 text-sm text-gray-600"
-          >
-            Full Name
-          </label>
+        <form onSubmit={handleSubmit}>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              onChange={handleEmail}
+              type="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="Your Email"
+              required
+            />
+            <p className="text-red-700 text-xs pt-1 pl-1">
+              {errors?.emailError}
+            </p>
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="password"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              onChange={handlePass}
+              type="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="Password"
+              required
+            />
+            <p className="text-red-700 text-xs pt-1 pl-1">
+              {errors?.passwordError}
+            </p>
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="confirmPassword"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Confirm Password
+            </label>
+            <input
+              onChange={handleConfirmPass}
+              type="password"
+              name="confirm-password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="Confirm Password"
+              required
+            />
+            <p className="text-red-700 text-xs pt-1 pl-1">
+              {errors?.confirmPasswordError}
+            </p>
+          </div>
+          <div className="">
+            <p>
+              Already have an account?{' '}
+              <span className="font-semibold text-blue-500 underline">
+                Log in
+              </span>
+            </p>
+          </div>
           <input
-            type="text"
-            name="full-name"
-            className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            placeholder="Your Name"
+            type="submit"
+            className="cursor-pointer text-white w-full text-center bg-purple-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-purple-600 rounded text-lg"
+            value="Register"
           />
-        </div>
-        <div className="relative mb-4">
-          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            placeholder="Your Email"
-          />
-        </div>
-        <div className="relative mb-4">
-          <label htmlFor="password" className="leading-7 text-sm text-gray-600">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            placeholder="Your Password"
-          />
-        </div>
-        <div className="">
-          <p>
-            Already have an account?{' '}
-            <span className="font-semibold text-blue-500 underline">
-              Log in
-            </span>
-          </p>
-        </div>
-        <button className="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
-          Register
-        </button>
+        </form>
         <SocialAuth />
       </div>
       <Footer />
